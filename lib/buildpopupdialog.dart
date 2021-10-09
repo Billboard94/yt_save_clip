a2ee1aa22fb73b87ff39c421de0d73cc1c6ea4c4
Billboard94 <valmir.ganiu@web.de>
2021-10-09 18:12:50 +0200

    added popup window widget

---
+++ lib/buildpopupdialog.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+Widget _buildPopupDialog(BuildContext context) {
+  return AlertDialog(
+    title: const Text('Popup example'),
+    content: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const <Widget>[
+        Text("Hello"),
+      ],
+    ),
+    actions: <Widget>[
+      TextButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        child: const Text('Close'),
+      ),
+    ],
+  );
+}
